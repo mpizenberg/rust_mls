fa3c68d5c1c4758dd80752630c2058250509ec23
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-09-22 13:38:48 +0200

    Warping with mls affine

--- moving-least-squares-demo/src/main.rs
+++ moving-least-squares-demo/src/main.rs
@@ -1,6 +1,8 @@
 use image::{GenericImageView, Pixel, RgbImage};
 use show_image::create_window;
 
+use moving_least_squares as mls;
+
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Open an image from disk.
@@ -10,22 +12,59 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Convert image into RGB.
     let mut img = img.into_rgb8();
 
-    // Draw some points.
+    // Define the source control points.
+    let controls_src: &[(f32, f32)] = &[
+        (40.0, 160.0),
+        (170.0, 160.0),
+        (320.0, 160.0),
+        (130.0, 280.0),
+        (220.0, 280.0),
+        (117.0, 369.0),
+        (250.0, 369.0),
+    ];
+
+    // Define the destination control points.
+    let controls_dst: &[(f32, f32)] = &[
+        (40.0, 190.0),
+        (170.0, 160.0),
+        (370.0, 120.0),
+        (110.0, 280.0),
+        (200.0, 280.0),
+        (117.0, 369.0),
+        (250.0, 369.0),
+    ];
+
+    // Draw source control points.
     let red: (u8, u8, u8) = (255, 0, 0);
-    let mut draw = |x, y, c| draw_point((x, y), 5.0, c, &mut img);
-    draw(40.0, 160.0, red); // top line
-    draw(170.0, 160.0, red);
-    draw(320.0, 160.0, red);
-    draw(130.0, 280.0, red); // middle line
-    draw(220.0, 280.0, red);
-    draw(117.0, 369.0, red); // bottom line
-    draw(250.0, 369.0, red);
+    controls_src
+        .iter()
+        .for_each(|&p| draw_point(p, 5.0, red, &mut img));
+
+    // Create new warped image.
+    let warped_img = RgbImage::from_fn(width, height, |x, y| {
+        let (x2, y2) = mls::deform_affine(controls_dst, controls_src, (x as f32, y as f32), 1e-10);
+        // nearest neighbor appoximation for now.
+        let u = x2.round() as i32;
+        let v = y2.round() as i32;
+        let u_inside = u >= 0 && u < width as i32;
+        let v_inside = v >= 0 && v < height as i32;
+        if u_inside && v_inside {
+            img.get_pixel(u as u32, v as u32).clone()
+        } else {
+            image::Rgb([0, 0, 0])
+        }
+    });
 
     // Create a window with default options and display the image.
     let window = create_window("image", Default::default())?;
     window.set_image("woody", img)?;
 
+    // Display new warped image in a new window.
+    let warped_window = create_window("warped image", Default::default())?;
+    warped_window.set_image("woody_warped", warped_img)?;
+
     window.wait_until_destroyed()?;
+    warped_window.wait_until_destroyed()?;
     Ok(())
 }
 

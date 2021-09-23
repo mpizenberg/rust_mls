ef5d7617c89cf074d159ad0e44c952d38c5aaf8c
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-09-23 19:41:58 +0200

    Extract nearest_neighbor function

--- moving-least-squares-image/src/lib.rs
+++ moving-least-squares-image/src/lib.rs
@@ -1,4 +1,4 @@
-use image::RgbImage;
+use image::{Rgb, RgbImage};
 
 use moving_least_squares as mls;
 
@@ -14,18 +14,10 @@ pub fn affine_reverse_dense(
     controls_dst: &[(f32, f32)],
 ) -> RgbImage {
     let (width, height) = img_src.dimensions();
+    let color_outside = Rgb([0, 0, 0]);
     RgbImage::from_fn(width, height, |x, y| {
         let (x2, y2) = mls::deform_affine(controls_dst, controls_src, (x as f32, y as f32));
-        // nearest neighbor appoximation for now.
-        let u = x2.round() as i32;
-        let v = y2.round() as i32;
-        let u_inside = u >= 0 && u < width as i32;
-        let v_inside = v >= 0 && v < height as i32;
-        if u_inside && v_inside {
-            img_src.get_pixel(u as u32, v as u32).clone()
-        } else {
-            image::Rgb([0, 0, 0])
-        }
+        nearest_neighbor(img_src, x2, y2).unwrap_or(color_outside)
     })
 }
 
@@ -41,18 +33,10 @@ pub fn similarity_reverse_dense(
     controls_dst: &[(f32, f32)],
 ) -> RgbImage {
     let (width, height) = img_src.dimensions();
+    let color_outside = Rgb([0, 0, 0]);
     RgbImage::from_fn(width, height, |x, y| {
         let (x2, y2) = mls::deform_similarity(controls_dst, controls_src, (x as f32, y as f32));
-        // nearest neighbor appoximation for now.
-        let u = x2.round() as i32;
-        let v = y2.round() as i32;
-        let u_inside = u >= 0 && u < width as i32;
-        let v_inside = v >= 0 && v < height as i32;
-        if u_inside && v_inside {
-            img_src.get_pixel(u as u32, v as u32).clone()
-        } else {
-            image::Rgb([0, 0, 0])
-        }
+        nearest_neighbor(img_src, x2, y2).unwrap_or(color_outside)
     })
 }
 
@@ -68,17 +52,23 @@ pub fn rigid_reverse_dense(
     controls_dst: &[(f32, f32)],
 ) -> RgbImage {
     let (width, height) = img_src.dimensions();
+    let color_outside = Rgb([0, 0, 0]);
     RgbImage::from_fn(width, height, |x, y| {
         let (x2, y2) = mls::deform_rigid(controls_dst, controls_src, (x as f32, y as f32));
-        // nearest neighbor appoximation for now.
-        let u = x2.round() as i32;
-        let v = y2.round() as i32;
-        let u_inside = u >= 0 && u < width as i32;
-        let v_inside = v >= 0 && v < height as i32;
-        if u_inside && v_inside {
-            img_src.get_pixel(u as u32, v as u32).clone()
-        } else {
-            image::Rgb([0, 0, 0])
-        }
+        nearest_neighbor(img_src, x2, y2).unwrap_or(color_outside)
     })
+}
+
+/// Retrieve nearest neighbor pixel for given coordinates.
+fn nearest_neighbor(img: &RgbImage, x: f32, y: f32) -> Option<Rgb<u8>> {
+    let (width, height) = img.dimensions();
+    let u = x.round() as i32;
+    let v = y.round() as i32;
+    let u_inside = u >= 0 && u < width as i32;
+    let v_inside = v >= 0 && v < height as i32;
+    if u_inside && v_inside {
+        Some(img.get_pixel(u as u32, v as u32).clone())
+    } else {
+        None
+    }
 }
